5feb59ffef0ccf500d87269c468947ad24ce7390
Andreas Pelme <andreas@pelme.se>
2023-06-06 10:41:38 +0200

    Move homebrew stuff out of nix-darwin. It just makes it slow and is not really needed to be run that often either way.

---
+++ Brewfile
@@ -0,0 +1,29 @@
+cask "1password"
+cask "arq"
+cask "aws-vault"
+cask "chromedriver"
+cask "discord"
+cask "firefox"
+cask "google-chrome"
+cask "google-drive"
+cask "istat-menus"
+cask "iterm2"
+cask "microsoft-teams"
+cask "poedit"
+cask "postico"
+cask "slack"
+cask "spotify"
+cask "teamviewer"
+cask "visual-studio-code"
+cask "yubico-authenticator"
+cask "yubico-yubikey-manager"
+cask "zoom"
+
+mas "BetterSnapTool", id: 417375580
+mas "ColorSlurp", id: 1287239339
+mas "Be Focused Pro - Focus Timer", id: 961632517
+mas "Messenger", id: 1480068668
+mas "Microsoft Word", id: 462054704
+mas "Microsoft Excel", id: 462058435
+mas "Microsoft PowerPoint", id: 462062816
+mas "Xcode", id: 497799835
--- configuration.nix
+++ configuration.nix
@@ -16,40 +16,6 @@
 
   security.pam.enableSudoTouchIdAuth = true;
 
-  homebrew.enable = true;
-  homebrew.casks = [
-      "1password"
-      "arq"
-      "discord"
-      "firefox"
-      "google-chrome"
-      "google-drive"
-      "istat-menus"
-      "iterm2"
-      "microsoft-teams"
-      "poedit"
-      "postico"
-      "slack"
-      "spotify"
-      "teamviewer"
-      "visual-studio-code"
-      "yubico-authenticator"
-      "yubico-yubikey-manager"
-      "zoom"
-      "sloth"
-  ];
-
-  homebrew.masApps = {
-      "BetterSnapTool" = 417375580;
-      "ColorSlurp" = 1287239339;
-      "Be Focused Pro - Focus Timer" = 961632517;
-      "Messenger" = 1480068668;
-      "Microsoft Word" = 462054704;
-      "Microsoft Excel" = 462058435;
-      "Microsoft PowerPoint" = 462062816;
-      "Xcode" = 497799835;
-  };
-
   fonts = {
       fontDir.enable = true;
       fonts = [
