868104800fe699dc93eef2a6acbf62288e750ece
Andreas Pelme <andreas@pelme.se>
2025-01-22 21:26:25 +0100

    Remove sapling.

--- home.nix
+++ home.nix
@@ -61,12 +61,6 @@
     };
   };
 
-  programs.sapling = {
-    enable = true;
-    userName = "Andreas Pelme";
-    userEmail = "andreas@pelme.se";
-  };
-
   programs.fish = {
     enable = true;
 
