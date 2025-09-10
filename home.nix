f1d8dcce31065549425c3d4ee8b2f7ad7e1ccfc9
Andreas Pelme <andreas@pelme.se>
2025-09-10 10:36:09 +0200

    Add ghostty config.

--- home.nix
+++ home.nix
@@ -113,6 +113,43 @@
       #{name = "grc"; src = grc.src;}
     ];
   };
+
+  programs.ghostty = {
+    enable = true;
+    package = null;
+    settings = {
+      font-family = "FiraCode Nerd Font Mono";
+      macos-titlebar-style = "tabs";
+      theme = "dracula";
+    };
+    themes.dracula = {
+      palette = [
+        "0=#21222c"
+        "1=#ff5555"
+        "2=#50fa7b"
+        "3=#f1fa8c"
+        "4=#bd93f9"
+        "5=#ff79c6"
+        "6=#8be9fd"
+        "7=#f8f8f2"
+        "8=#6272a4"
+        "9=#ff6e6e"
+        "10=#69ff94"
+        "11=#ffffa5"
+        "12=#d6acff"
+        "13=#ff92df"
+        "14=#a4ffff"
+        "15=#ffffff"
+      ];
+      background = "#212121";
+      foreground = "#f8f8f2";
+      cursor-color = "#f8f8f2";
+      cursor-text = "#282a36";
+      selection-foreground = "#f8f8f2";
+      selection-background = "#44475a";
+    };
+  };
+
   programs.helix = {
     enable = true;
     defaultEditor = true;
